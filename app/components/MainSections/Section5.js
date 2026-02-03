0a6c77bc9920416373ad5b0b82519b1c7c6f2ef3
Traversal GEU <traversalgeu15@gmail.com>
2026-02-03 19:46:31 +0530

    Update Section5.js

--- app/components/MainSections/Section5.js
+++ app/components/MainSections/Section5.js
@@ -17,7 +17,7 @@ export default function Section5() {
         </div>
 
         {/* Faculty Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
           {facultyData.map((faculty) => (
             <div
               data-aos="fade-up" data-aos-duration="2200"
