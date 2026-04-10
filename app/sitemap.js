bd3276446464012d806355ac857e0fb70c640f53
Ayush Rawat <ayushrf@gmail.com>
2026-04-10 21:21:22 +0530

    SEO Refining

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.traversalhub.in/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.traversalhub.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.traversalhub.in/events",
+      lastModified: new Date(),
+    },
+  ];
+}
